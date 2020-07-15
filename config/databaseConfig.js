fc0f8cd9981300968e8134b11ded4d8f63673f2f
Jose De La Rosa <josedlr93@aol.com>
2020-07-15 11:27:37 -0400

    Refactor app for future testing

---
+++ config/databaseConfig.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  const result = dotenv.config();
+  
+  if(result.error) {
+    throw result.error;
+  }
+  console.log(result.parsed);
+}
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-ascvt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+export default DB_URI;

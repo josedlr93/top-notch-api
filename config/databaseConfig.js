f2bf2537dd0ed53684ce5be966faf366870ae23d
Jose De La Rosa <josedlr93@aol.com>
2020-11-10 16:46:04 -0500

    Update databaseConfig.js for local mongo instance

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,17 +1,24 @@
 import dotenv from 'dotenv';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   const result = dotenv.config();
   
   if(result.error) {
     throw result.error;
   }
-  console.log('databaseConfig.js: Not from production');
+  console.log('databaseConfig.js: Development');
 }
 
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
-const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-ascvt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+let DB_URI = '';
+
+if (process.env.NODE_ENV === 'production') {
+  DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-ascvt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+} else {
+  DB_URI = `mongodb://localhost:27017/test`;
+}
 
 export default DB_URI;

5824c226046d2c7ad779ff141aa6939f7e3f1152
Jose De La Rosa <josedlr93@aol.com>
2020-11-18 16:03:29 -0500

    Modify employeeController.js - import and use employeeService.js Add tests - employeeRoutes.test.js

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -1,68 +1,54 @@
 import mongoose from 'mongoose';
+
 import { EmployeeSchema } from '../models/employeeModel.js';
-import { handleDuplicateKey } from '../lib/errorHandler.js';
+import * as employeeService from '../services/employeeService.js';
 
 const Employee = mongoose.model('Employee', EmployeeSchema);
 
-export const addNewEmployee = (req, res) => {
-  let newEmployee = new Employee(req.body);
-
-  newEmployee.save((err, employee) => {
-    if (err) {
-      // MongoError: Duplicate key error
-      if (err.code === 11000) {
-        handleDuplicateKey(res, err);
-      } else {
-        res.send(err);
-      }
-    }
+export const addNewEmployee = async (req, res, next) => {
+  try {
+    const employee = await employeeService.addNewEmployee(Employee, req.body);
     res.json(employee);
-  });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getEmployees = (req, res) => {
-  Employee.find({}, (err, employee) => {
-    if (err) {
-      res.send(err);
-    }
+export const getEmployees = async (req, res, next) => {
+  try {
+    const employees = await employeeService.getEmployees(Employee);
+    res.json(employees);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getEmployeeWithID = async (req, res, next) => {
+  try {
+    const employee = await employeeService.getEmployeeWithID(Employee, req.params.employeeID);
     res.json(employee);
-  });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getEmployeeWithID = (req, res) => {
-  Employee.findById(req.params.employeeID, (err, employee) => {
-    if (err) {
-      res.send(err);
-    }
+export const updateEmployee = async (req, res, next) => {
+  try {
+    const employee = await employeeService.updateEmployee(Employee, req.params.employeeID, req.body);
     res.json(employee);
-  });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const updateEmployee = (req, res) => {
-  Employee.findOneAndUpdate(
-    { _id: req.params.employeeID },
-    req.body,
-    {
-      new: true,
-      useFindAndModify: false
-    },
-    (err, employee) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json(employee);
-    });
-};
-
-export const deleteEmployee = (req, res) => {
-  Employee.findByIdAndDelete({ _id: req.params.employeeID }, (err, employee) => {
-    if (err) {
-      res.send(err);
-    }
-    console.log(employee);
+export const deleteEmployee = async (req, res, next) => {
+  try {
+    const employee = await employeeService.deleteEmployee(Employee, req.params.employeeID);
     res.json({
       employee,
       message: employee ? 'Successfully deleted employee' : `No employee with ID: ${req.params.employeeID}`
     });
-  });
+  } catch (err) {
+    next(err);
+  }
 };
